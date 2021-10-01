68e4dd5cbb5760c802edcff17288c22762f4ac03
mirore52 <michael.r.r52@gmail.com>
2021-10-01 14:13:15 +0200

    Added coleccion_3

--- tema_2/coleccion2/ejercicio_2/index.php
+++ tema_2/coleccion2/ejercicio_2/index.php
@@ -17,6 +17,60 @@ Se pide realizar un formulario que nos pida el número de expediente y nos visua
 
     <?php
 
+        $students_array = ["EDAW101"=>"María Pérez Morales", "EDAW102" => "Juan Arguedas Sierra", "EDAW103" => "Mario García Jiménez"];
+    
+        function printForm(){
+            echo<<<END
+            <div>
+            <form action="." method="GET" enctype="application/x-www-form-urlencoded">
+                <label for="student_id">Introduzca el número de expediente (EDAW10_): </label>
+                <input type="text" id="student_id" name="student_id">
+                <input type="submit">
+            </form>
+            <br>
+            </div>
+END;
+        }
+    
+        function searchArray($student_id, $students_array){   
+            if(isset($students_array[$student_id])){
+                printSearch($students_array[$student_id]);
+            }else {
+                $error_message = "Expediente no encontrado";
+                printError($error_message);
+            } 
+        }
+    
+        function printSearch($student_name){
+            echo "<div>";
+                echo "<span>Nombre del estudiante: </span><br><br>";
+                echo $student_name;       
+                echo "<br>";
+                echo "<br>";
+                echo "<button class='btn'><a href='.'>Volver</a></button>";
+            echo "</div>";
+        }
+    
+        function printError($error_message){
+            echo "<div>";           
+                echo "<span>$error_message</span>";    
+                echo "<br>";
+                echo "<br>";
+                echo "<button class='btn'><a href='.'>Volver</a></button>";
+            echo "</div>";
+        }
+    
+        if(!isset($_GET['student_id'])){
+            printForm();
+            echo "<br><br>";
+            echo "<button class='bottom-btn'><a href='..'>Atrás</a></button>";
+        }else if(empty($_GET['student_id'])){
+            $error_message = "Introduzca un expediente";
+            printError($error_message);
+        }else {
+            searchArray($_GET['student_id'], $students_array);
+        }
+/*
     $students_array = ["EDAW101"=>"María Pérez Morales", "EDAW102" => "Juan Arguedas Sierra", "EDAW103" => "Mario García Jiménez"];
     
     function printForm(){
@@ -31,14 +85,14 @@ Se pide realizar un formulario que nos pida el número de expediente y nos visua
         </div>
 END;
     }
-
-    function printSearch($array){
+  
+    function printSearch($students_array){
         echo "<div>";           
-            if(!searchArray($_GET['student_id'], $array)){
+            if(!searchArray($_GET['student_id'], $students_array)){
                 echo "<span>Expediente no encontrado</span>";
             }else{
                 echo "<span>Nombre del estudiante: </span><br><br>";
-                echo searchArray($_GET['student_id'], $array);
+                echo searchArray($_GET['student_id'], $students_array);
             }         
             echo "<br>";
             echo "<br>";
@@ -46,9 +100,9 @@ END;
         echo "</div>";
     }
 
-    function searchArray($str, $array){   
-        if(isset($array[$str])){
-            return $array[$str];
+    function searchArray($str, $students_array){   
+        if(isset($students_array[$str])){
+            return $students_array[$str];
         } else return false;
     }
 
@@ -58,7 +112,7 @@ END;
         echo "<button class='bottom-btn'><a href='..'>Atrás</a></button>";
     }else {
         printSearch($students_array);
-    }
+    }*/
     ?>
 
 </body>
