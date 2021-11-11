c01c70538a538408a85c8c136965792ac8c6bdb9
alvaro <a.garciatorr@gmail.com>
2021-11-11 23:02:41 +0100

    11/11

--- tema_GD/ejercicio_2/index.php
+++ tema_GD/ejercicio_2/index.php
@@ -9,15 +9,21 @@ function printForm(){
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Ejercicio 2</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
     <body>
         <h1>Pinta un cuadrado del color que quieras</h1>
         <div>
-            <form action="." method="POST" enctype="application/x-www-form-urlencoded">
+            <form action="php/img.php" method="POST" enctype="application/x-www-form-urlencoded">
                 <label for="user_color"></label>
                 <input type="color" name="user_color" id="user_color">
                 <input type="submit">
             </form>
+END;
+    if(isset($_GET['image'])){
+            printImg();
+    }
+    echo<<<END
         </div>   
     </body>
     </html>
@@ -25,32 +31,15 @@ END;
 }
 
 function printImg(){
-    var_dump($_POST);
-    echo<<<END
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ejercicio 2</title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-    </head>
-    <body>
-        <div style="background-color: grey; padding: 1rem">
-END;
-    echo "<img src='php/img.php?color=" . $_POST['user_color'] . "' style='margin:  0 43.8%'>";
-    echo "</div>";
-    echo "</body>";
-    echo "</html>";
+    echo "<img src='img/img.png' style='margin:  0 35%'>";
 }
 
 
 
-if(!isset($_POST['user_color'])){
+
+ if(isset($_GET['image'])){
     printForm();
-} else {
-    printImg();
+} else if(!isset($_POST['user_color'])){
+    printForm();
 }
-
 ?>
--- tema_GD/ejercicio_2/php/img.php
+++ tema_GD/ejercicio_2/php/img.php
@@ -1,7 +1,5 @@
 <?php
 
-header("Content-Type: image/png");
-
 $img = imagecreatetruecolor(200, 200);
 
 $blanco = imagecolorallocate($img, 255, 255, 255);
@@ -9,15 +7,13 @@ $black = imagecolorallocate($img, 0, 0, 0);
 $red = imagecolorallocate($img, 255, 0, 0);
 
 
-$rgb = str_split(substr($_GET['color'], 1), 2);
+$rgb = str_split(substr($_POST['user_color'], 1), 2);
 //$rgb = array_map("hexdec", $rgb);
 $r = hexdec($rgb[0]);
 $g = hexdec($rgb[1]);
-//$b = hexdec($rgb[2]);
+$b = hexdec($rgb[2]);
 
-$user_color = imagecolorallocate($img, 100, 200, 100);
-
-//$user_color = imagecolorallocate($img, "0xFF", "0xFF", "0xFF");
+$user_color = imagecolorallocate($img, $r, $g, $b);
 
 imagefill($img, 0, 0, $blanco);
 
@@ -25,6 +21,12 @@ imagefilledrectangle($img, 50, 50, 150, 150, $user_color);
 
 imagerectangle($img, 50, 50, 150, 150, $black);
 
-imagepng($img);
+//header("Content-Type: image/png");
+$file = "../img/img.png";
+imagepng($img, $file);
+
+//imagegd($img, "img/img.png");
+
+header("Location: ..?image=true");
 
 ?>
